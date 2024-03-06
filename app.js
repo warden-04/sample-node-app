1d651a10832480919c85a388aa44455194249459
warden-04 <abc1@gmail.com>
2024-03-06 07:00:28 +0530

    Adding Project files

---
+++ app.js
@@ -0,0 +1,15 @@
+// app.js
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3010;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
